8387480ccecaadc5e2f7c444eff14aa3dcfff74b
Lars Mogren <lars.mogren@gmail.com>
2017-11-07 16:18:10 +0100

    Tested on Vagrant.

--- add-server.sh
+++ add-server.sh
@@ -4,23 +4,26 @@
 
 set -ex
 
-host=$1
+server=$1
 interface=$2
 tincIP=$3
-tincName=${4:-$1}
+tincName=$4
+orgDefaultGateway=$5
 
-if [ ! "$1" ]; then echo "host is required as argument #1"; exit 1; fi
+if [ ! "$1" ]; then echo "server (IP or hostname) is required as argument #1"; exit 1; fi
 if [ ! "$2" ]; then echo "network interface is required as argument #2"; exit 1; fi
 if [ ! "$3" ]; then echo "IP-address required as argument #3"; exit 1; fi
+if [ ! "$4" ]; then echo "tinc name is required as argument #4"; exit 1; fi
+if [ ! "$5" ]; then echo "default gateway to resore is required as argument #5"; exit 1; fi
 
 vpnName=scaleway
 tincPath=/etc/tinc/$vpnName
 
-myTincIP=$(ip addr show tun0 | grep -o 'inet [^/]*' | cut -d' ' -f 2)
+tincGwIP=$(ip addr show tun0 | grep -o 'inet [^/]*' | cut -d' ' -f 2)
 myIP=$(ip addr show $interface | grep -o 'inet [^/]*' | cut -d' ' -f 2)
 myName=$(cat $tincPath/tinc.conf | awk '/Name/ { print $3}')
 
-rsync -Pavvzessh /var/cache/apt/archives/tinc_*.deb root@${host}:/tmp
+rsync -Pavvzessh /var/cache/apt/archives/tinc_*.deb root@${server}:/tmp
 
 remoteScriptFile=$(mktemp)
 chmod +x $remoteScriptFile
@@ -35,12 +38,13 @@ if [ ! -f ${tincPath}/tinc.conf ]; then
 Name = ${tincName}
 AddressFamily = ipv4
 Interface = tun0
+Mode = switch
 ConnectTo = ${myName}
 END
 fi
 if [ ! -f ${tincPath}/hosts/${tincName} ]; then
 	cat <<END > ${tincPath}/hosts/${tincName}
-Subnet = ${tincIP}/32
+#Subnet = ${tincIP}/32
 END
 	tincd -n $vpnName -K4096
 fi
@@ -48,35 +52,28 @@ fi
 cat <<'END' > ${tincPath}/tinc-up
 #!/bin/sh
 ifconfig INTERFACE ${tincIP} netmask 255.255.255.0
+route add -host ${myIP} gw ${orgDefaultGateway}
+route add default gw ${tincGwIP}
+route del default gw ${orgDefaultGateway}
 END
 
 cat <<'END' > ${tincPath}/tinc-down
 #!/bin/sh
 ifconfig INTERFACE down
+route add default gw ${orgDefaultGateway}
+route del -host ${myIP} gw ${orgDefaultGateway}
+route del default gw ${tincGwIP}
 END
 
 chmod +x ${tincPath}/tinc-{up,down}
 modprobe tun
 EOF
 sed -i -e 's/INTERFACE/$INTERFACE/g' $remoteScriptFile
-scp $remoteScriptFile root@$host:~/setup-tinc.sh
-ssh root@$host 'bash -x ~/setup-tinc.sh'
+scp $remoteScriptFile root@$server:~/setup-tinc.sh
+ssh root@$server 'bash -x ~/setup-tinc.sh'
 
-rsync -Pavvzessh ${tincPath}/hosts/$myName $host:${tincPath}/hosts/
-rsync -Pavvzessh $host:${tincPath}/hosts/${tincName} ${tincPath}/hosts/
+rsync -Pavvzessh ${tincPath}/hosts/$myName $server:${tincPath}/hosts/
+rsync -Pavvzessh $server:${tincPath}/hosts/${tincName} ${tincPath}/hosts/
 tincd -n $vpnName -k HUP || tincd -n $vpnName
 
-ssh root@$host "tincd -n $vpnName -k HUP || tincd -n $vpnName"
-
-routeScriptFile=$(mktemp)
-chmod +x $routeScriptFile
-cat <<EOF > $routeScriptFile
-#!/bin/bash
-tincd -n $vpnName -k HUP || tincd -n $vpnName
-GATEWAY=\$(route -n | grep '^0.0.0.0' | awk '{print \$2}')
-route add -host $myIP gw \$GATEWAY
-route add default gw $myTincIP
-route del default gw \$GATEWAY
-EOF
-scp $routeScriptFile root@$host:~/setup-default-gw.sh
-ssh root@$host 'bash -x ~/setup-default-gw.sh'
+ssh root@$server "tincd -n $vpnName -k HUP || tincd -n $vpnName"
--- setup-gw.sh
+++ setup-gw.sh
@@ -20,34 +20,63 @@ if [ ! -f "$tincPath/tinc.conf" ]; then
 Name = ${tincName}
 AddressFamily = ipv4
 Interface = tun0
+Mode = switch
 EOF
 fi
 
 if [ ! -f "$tincPath/hosts/$tincName" ]; then
     cat <<EOF > $tincPath/hosts/$tincName
 Address = ${myIP}
-Subnet = 0.0.0.0/0
+#Subnet = 0.0.0.0/0
 EOF
 tincd -n $vpnName -K4096
 fi
 
+if0=tun0
+if1=${interface}
+
 cat <<EOF > $tincPath/tinc-up
 #!/bin/bash
+
 ifconfig \$INTERFACE ${tincIP} netmask 255.255.255.0
+
+#
+# delete all existing rules.
+#
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+iptables -X
+
+# Always accept loopback traffic
+iptables -A INPUT -i lo -j ACCEPT
+
+
+# Allow established connections, and those not coming from the outside
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state NEW ! -i $if1 -j ACCEPT
+iptables -A FORWARD -i ${if1} -o ${if0} -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allow outgoing connections from the LAN side.
+iptables -A FORWARD -i ${if0} -o ${if1} -j ACCEPT
+
+# Masquerade.
+iptables -t nat -A POSTROUTING -o ${if1} -j MASQUERADE
+
+# Don't forward from the outside to the inside.
+iptables -A FORWARD -i ${if1} -o ${if1} -j REJECT
+
+# Enable routing.
+echo 1 > /proc/sys/net/ipv4/ip_forward
 EOF
 
 cat <<EOF > $tincPath/tinc-down
 #!/bin/bash
 ifconfig \$INTERFACE down
+iptables -F
 EOF
 
 chmod +x $tincPath/tinc-{up,down}
 
 modprobe tun
 tincd -n $vpnName -k HUP || tincd -n $vpnName
-
-# And set up forwarding/nat
-echo 1 > /proc/sys/net/ipv4/ip_forward
-/sbin/iptables -t nat -A POSTROUTING -o $interface -j MASQUERADE
-/sbin/iptables -A FORWARD -i $interface -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -A FORWARD -i tun0 -o $interface -j ACCEPT
